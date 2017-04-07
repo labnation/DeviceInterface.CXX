21c157a9f8c0cb6e01dde22ef872a11f6fff1ec5
Jasper van Bourgognie <van.bourgognie@gmail.com>
2017-04-07 14:52:40 +0200

    fix openwrt environment init for multiple toolchains

--- openwrt.sh
+++ openwrt.sh
@@ -3,4 +3,4 @@ export TARGET=OPENWRT
 export OPENWRT_PATH=~/labnation/openwrt/openwrt/
 export STAGING_DIR=$OPENWRT_PATH/staging_dir
 export CROSS_COMPILE=${STAGING_DIR}/toolchain-mipsel_24kec+dsp_gcc-4.8-linaro_uClibc-0.9.33.2/bin/mipsel-openwrt-linux-uclibc-
-export PREFIX=`realpath $STAGING_DIR/target-*/usr/`
+export PREFIX=`realpath $STAGING_DIR/target-*uClibc*/usr/`
