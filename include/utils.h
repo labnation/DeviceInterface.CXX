e944876cc734eb7b195dd289fb42cc563fd79acc
Jasper van Bourgognie <van.bourgognie@gmail.com>
2017-04-07 14:52:25 +0200

    fix in utils.h and add member_size macro

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,8 @@
 #ifndef _LABNATION_UTILS_H
 #define _LABNATION_UTILS_H
 
-#endif // _LABNATION_UTILS_H
-
 int pthread_join_timeout(pthread_t wid, unsigned int msecs);
+
+#define member_size(type, member) sizeof(((type *)0)->member)
+
+#endif // _LABNATION_UTILS_H
