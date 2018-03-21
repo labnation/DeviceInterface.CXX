6008a402e0effc015ebf9f6342702dd090991ad0
Jasper van Bourgognie <van.bourgognie@gmail.com>
2018-03-21 13:37:19 +0100

    implement channels

--- include/labnation/channel.h
+++ include/labnation/channel.h
@@ -2,6 +2,8 @@
 #define _LABNATION_CHANNEL_H
 
 #include <typeinfo>
+#include <string>
+#include <vector>
 
 namespace labnation {
 
@@ -10,19 +12,52 @@ class Channel {
 public:
   std::string name;
   uint32_t value;
-  std::type_info* datatype;
+  const std::type_info* datatype;
+  static std::vector<Channel> list;
 
-  Channel(std::string name, uint32_t value, std::type_info* datatype)
+  Channel(std::string name, uint32_t value, const std::type_info * datatype)
   {
     this->name = name;
     this->value = value;
-    this->datatype = datatype;
+    this->datatype =  datatype;
+
+    list.push_back(*this);
   }
 };
 
 class AnalogChannel: Channel {
 
+public:
+  static std::vector<AnalogChannel> list;
+
+  AnalogChannel(std::string name, uint32_t value) : Channel(name, value, &typeid(float)) {
+    list.push_back(*this);
+  }
+
 };
+
+class DigitalChannel: Channel {
+
+public:
+  static std::vector<DigitalChannel> list;
+
+  DigitalChannel(std::string name, uint32_t value) : Channel(name, value, &typeid(bool)) {
+    DigitalChannel::list.push_back(*this);
+  }
+
+};
+
+namespace channels {
+  static AnalogChannel ChA = AnalogChannel("A", 0);
+  static DigitalChannel Digi0 = DigitalChannel("0", 0);
+  static DigitalChannel Digi1 = DigitalChannel("1", 1);
+  static DigitalChannel Digi2 = DigitalChannel("2", 2);
+  static DigitalChannel Digi3 = DigitalChannel("3", 3);
+  static DigitalChannel Digi4 = DigitalChannel("4", 4);
+  static DigitalChannel Digi5 = DigitalChannel("5", 5);
+  static DigitalChannel Digi6 = DigitalChannel("6", 6);
+  static DigitalChannel Digi7 = DigitalChannel("7", 7);
+}
 
 }
 #endif // _LABNATION_CHANNEL_H
---
+++ src/channel.cpp
@@ -0,0 +1,10 @@
+#include <labnation/channel.h>
+#include <vector>
+
+namespace labnation {
+
+std::vector<Channel>        Channel::list;
+std::vector<DigitalChannel> DigitalChannel::list;
+std::vector<AnalogChannel>  AnalogChannel::list;
+
+}
