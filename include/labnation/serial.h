d7d6dfcf5ec12d60ccb5f5c9610a2ab9deb0b399
Jasper van Bourgognie <van.bourgognie@gmail.com>
2018-03-22 19:55:48 +0100

    implement basic serial

---
+++ include/labnation/serial.h
@@ -0,0 +1,58 @@
+#ifndef _LABNATION_SERIAL_H
+#define _LABNATION_SERIAL_H
+
+#include <string>
+
+#define LIST_PLANT \
+  X(UNKNOWN, -1) \
+  X(TOLSTRAAT, 0) \
+  X(POPERINGE, 1) \
+  X(ORADEA, 2)
+
+#define X(name, value) PLANT_##name = value,
+enum Plant { LIST_PLANT };
+#undef X
+
+#define MODEL_OFFSET      (36*36)
+#define MODEL_SMARTSCOPE  (0xA)
+#define GENERATION_OFFSET (36)
+
+#define LIST_MODEL \
+    X(UNKNOWN,-1) \
+    X(SMARTSCOPE_A00, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (0 * GENERATION_OFFSET) + 0) \
+    X(SMARTSCOPE_A10, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 0) \
+    X(SMARTSCOPE_A11, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 1) \
+    X(SMARTSCOPE_A12, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 2) \
+    X(SMARTSCOPE_A14, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 4) \
+    X(SMARTSCOPE_A15, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 5) \
+    X(SMARTSCOPE_A16, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 6) \
+    X(SMARTSCOPE_A17, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 7) \
+    X(SMARTSCOPE_A18, (MODEL_SMARTSCOPE * MODEL_OFFSET) + (1 * GENERATION_OFFSET) + 8)
+
+#define X(name, value) MODEL_##name = value,
+enum Model { LIST_MODEL};
+#undef X
+
+namespace labnation
+{
+
+class Serial {
+
+public:
+  Serial(std::string serial);
+  static void Explain(Serial::Serial s);
+
+  Plant plant;
+  Model model;
+  int week;
+  int year;
+  long number;
+
+private:
+  std::string source;
+
+};
+
+}
+
+#endif // _LABNATION_SERIAL_H
---
+++ src/serial.cpp
@@ -0,0 +1,34 @@
+#include <labnation.h>
+#include <labnation/serial.h>
+#include <utils.h>
+#include <time.h>
+#include <string>
+#include <map>
+
+namespace labnation {
+
+#define X(name, value) { value, #name },
+std::map<int, const char*> PlantMap = { LIST_PLANT };
+std::map<int, const char*> ModelMap = { LIST_MODEL };
+#undef X
+
+Serial::Serial(std::string serial) {
+  source = serial;
+
+  plant = (Plant)Base36::Decode(serial.substr(0, 2));
+  year = 2010 + std::stoi(serial.substr(2, 1));
+  week = std::stoi(serial.substr(3, 2));
+  number= Base36::Decode(serial.substr(5, 3));
+  model = (Model)Base36::Decode(serial.substr(8, 3));
+}
+
+void Serial::Explain(Serial::Serial s) {
+  info("Serial %s: ", s.source.c_str());
+  info("   Plant    %s [%d]", PlantMap[s.plant], s.plant);
+  info("   Year     %4d", s.year);
+  info("   Week     %d", s.week);
+  info("   Number   %lu", s.number);
+  info("   Model    %s [%d]", ModelMap[s.model], s.model);
+}
+
+}
