1537e01ed5d181a6a68e3f4ac78616392f3c71e3
Jasper van Bourgognie <van.bourgognie@gmail.com>
2018-03-23 21:36:34 +0100

    register: clean debug msgs

--- src/memory/register.cpp
+++ src/memory/register.cpp
@@ -13,8 +13,6 @@ Register::Register(void* m, uint32_t address, const char* name)
 }
 
 void Register::Write() {
-  debug("Writing register %s [0x%02X] (%d)", name.c_str(), address, dirty);
-  //static_cast<Memory*>(memory)->write(address);
   memory->Write(address);
 }
 
@@ -24,8 +22,6 @@ void Register::Write(uint32_t value) {
 }
 
 Register* Register::Read() {
-  debug("Reading register %s [0x%02X] (%d)", name.c_str(), address, dirty);
-  //static_cast<Memory*>(memory)->read(address);
   memory->Read(address);
   return this;
 }
@@ -35,7 +31,6 @@ uint32_t Register::Get() {
 }
 
 void Register::Set(uint32_t value) {
-  debug("Setting reg %s to 0x%02X", name.c_str(), value);
   _value = value;
   dirty = true;
 }
